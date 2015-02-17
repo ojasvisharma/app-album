01bb24785adb44599541b8bdde0af3afdd763747
Ojasvi <ojasvi@ojasvi-Latitude-3540.(none)>
2015-02-17 10:26:02 +0530

    Second Commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'albums#new'
 
-
+resources :pictures
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-     resources :albums
+     resources :albums 
      resources :albums do
         resources :pictures
      end
