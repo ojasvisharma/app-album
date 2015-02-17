2521a95da0ca1aad2c21c7aa18c6a0c73784998d
Ojasvi <ojasvi@ojasvi-Latitude-3540.(none)>
2015-02-17 12:35:43 +0530

    4 Commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,17 +11,17 @@ Rails.application.configure do
     
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default :charset => "utf-8"
 
 config.action_mailer.smtp_settings = {
 address: "smtp.gmail.com",
 port: 587,
-domain: ENV["GMAIL_DOMAIN"],
+domain: "gmail.com",
 authentication: "plain",
 enable_starttls_auto: true,
-user_name: ENV["GMAIL_USERNAME"],
-password: ENV["GMAIL_PASSWORD"]
+user_name: "osharma.enbake@gmail.com",
+password: "Welcome1@"
 }
 
   # Eager load code on boot. This eager loads most of Rails and
